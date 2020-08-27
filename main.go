6cafc1064b13274b613b965c684f31da15abe1a0
Jorrit Salverda <jsalverda@travix.com>
2020-08-27 14:43:45 +0200

    compare reading to previous and skip if too large

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"runtime"
 	"time"
 
 	"github.com/alecthomas/kingpin"
 	foundation "github.com/estafette/estafette-foundation"
 	"github.com/rs/zerolog/log"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 var (
@@ -28,6 +35,9 @@ var (
 	bigqueryProjectID = kingpin.Flag("bigquery-project-id", "Google Cloud project id that contains the BigQuery dataset").Envar("BQ_PROJECT_ID").Required().String()
 	bigqueryDataset   = kingpin.Flag("bigquery-dataset", "Name of the BigQuery dataset").Envar("BQ_DATASET").Required().String()
 	bigqueryTable     = kingpin.Flag("bigquery-table", "Name of the BigQuery table").Envar("BQ_TABLE").Required().String()
+
+	measurementFilePath          = kingpin.Flag("state-file-path", "Path to file with state.").Default("/configs/last-measurement.json").OverrideDefaultFromEnvar("MEASUREMENT_FILE_PATH").String()
+	measurementFileConfigMapName = kingpin.Flag("state-file-configmap-name", "Name of the configmap with state file.").Default("sunny-bigquery-exporter").OverrideDefaultFromEnvar("MEASUREMENT_FILE_CONFIG_MAP_NAME").String()
 )
 
 func main() {
@@ -47,6 +57,20 @@ func main() {
 	// init bigquery table if it doesn't exist yet
 	initBigqueryTable(bigqueryClient)
 
+	// create kubernetes api client
+	kubeClientConfig, err := rest.InClusterConfig()
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	// creates the clientset
+	kubeClientset, err := kubernetes.NewForConfig(kubeClientConfig)
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+
+	// get previous measurement
+	measurementMap := readLastMeasurementFromMeasurementFile()
+
 	client, err := NewSunnyBoyClient(*sunnyHostIPAddress, *sunnyHostPort, *sunnyUnitID)
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed creating sunny boy client")
@@ -57,10 +81,16 @@ func main() {
 		log.Fatal().Err(err).Msg("Failed reading totalWhOut")
 	}
 
+	name := "Sunny TriPower 8.0"
+	// if difference with previous measurement is too large ( > 10 kWh ) ignore, the p1 connection probably returned an incorrect reading
+	if previousValueAsFloat64, ok := measurementMap[name]; ok && float64(totalWhOut)-previousValueAsFloat64 > 10*1000 {
+		log.Fatal().Msgf("Increase for reading '%v' is %v, more than the allowed 10 kWh, skipping the reading", name, float64(totalWhOut)-previousValueAsFloat64)
+	}
+
 	measurement := BigQueryMeasurement{
 		Readings: []BigQueryInverterReading{
 			{
-				Name:    "Sunny TriPower 8.0",
+				Name:    name,
 				Reading: float64(totalWhOut),
 				Unit:    "Wh",
 			},
@@ -72,10 +102,10 @@ func main() {
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed inserting measurements into bigquery table")
 	}
-	log.Info().Msgf("Stored %v readings, exiting...", len(measurement.Readings))
 
-	// done
-	log.Info().Msg("Finished exporting metrics")
+	writeMeasurementToConfigmap(kubeClientset, measurement)
+
+	log.Info().Msgf("Stored %v readings, exiting...", len(measurement.Readings))
 }
 
 func initBigqueryTable(bigqueryClient BigQueryClient) {
@@ -95,4 +125,68 @@ func initBigqueryTable(bigqueryClient BigQueryClient) {
 			log.Fatal().Err(err).Msg("Failed updating bigquery table schema")
 		}
 	}
+}
+
+func readLastMeasurementFromMeasurementFile() (measurementMap map[string]float64) {
+
+	measurementMap = map[string]float64{}
+
+	// check if last measurement file exists in configmap
+	var lastMeasurement BigQueryMeasurement
+	if _, err := os.Stat(*measurementFilePath); !os.IsNotExist(err) {
+		log.Info().Msgf("File %v exists, reading contents...", *measurementFilePath)
+
+		// read state file
+		data, err := ioutil.ReadFile(*measurementFilePath)
+		if err != nil {
+			log.Fatal().Err(err).Msgf("Failed reading file from path %v", *measurementFilePath)
+		}
+
+		log.Info().Msgf("Unmarshalling file %v contents...", *measurementFilePath)
+
+		// unmarshal state file
+		if err := json.Unmarshal(data, &lastMeasurement); err != nil {
+			log.Fatal().Err(err).Interface("data", data).Msg("Failed unmarshalling last measurement file")
+		}
+
+		for _, r := range lastMeasurement.Readings {
+			measurementMap[r.Name] = r.Reading
+		}
+	}
+
+	return measurementMap
+}
+
+func writeMeasurementToConfigmap(kubeClientset *kubernetes.Clientset, measurement BigQueryMeasurement) {
+
+	// retrieve configmap
+	configMap, err := kubeClientset.CoreV1().ConfigMaps(getCurrentNamespace()).Get(*measurementFileConfigMapName, metav1.GetOptions{})
+	if err != nil {
+		log.Error().Err(err).Msgf("Failed retrieving configmap %v", *measurementFileConfigMapName)
+	}
+
+	// marshal state to json
+	measurementData, err := json.Marshal(measurement)
+	if configMap.Data == nil {
+		configMap.Data = make(map[string]string)
+	}
+
+	configMap.Data[filepath.Base(*measurementFilePath)] = string(measurementData)
+
+	// update configmap to have measurement available when the application runs the next time and for other applications
+	_, err = kubeClientset.CoreV1().ConfigMaps(getCurrentNamespace()).Update(configMap)
+	if err != nil {
+		log.Fatal().Err(err).Msgf("Failed updating configmap %v", *measurementFileConfigMapName)
+	}
+
+	log.Info().Msgf("Stored measurement in configmap %v...", *measurementFileConfigMapName)
+}
+
+func getCurrentNamespace() string {
+	namespace, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed reading namespace")
+	}
+
+	return string(namespace)
 }
